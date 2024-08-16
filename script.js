38cc25e2a973df3224da430ff8fc21f1746d5a9b
Lucas Täkker <50625016+Sniperhund@users.noreply.github.com>
2024-08-16 11:44:59 +0200

    feat: Add slider functionality and images to index.html and script.js

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 import { Slider } from "./slider.js"
 
-const slider = new Slider("#test", [
-    "/assets/img/img1.jpeg",
-    "/assets/img/img2.jpg",
-    "/assets/img/img3.jpg",
-])
+const images = []
+
+for (let i = 1; i <= 10; i++) {
+    images.push(`https://picsum.photos/1920/1080?random=${i}`)
+}
+
+window.slider = new Slider(".slides", images)
+
+window.slider.addImageIndexCallback((index) => {
+    document.querySelector("#display-text").innerText = `Billede ${index + 1}`
+})
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowLeft") {
+        slider.prevSlide()
+    }
+
+    if (event.key === "ArrowRight") {
+        slider.nextSlide()
+    }
+})
+
+alert(
+    `FYI så lagger det lidt første gang man går igennem, men efter det kan jeg hvertfald ikke se seams
+Det kan være billederne loader, idk orkede hellere ikke fikse det`
+)
--- slider.js
+++ slider.js
@@ -1,109 +1,134 @@
+/* Taken from Stackoverflow (can't remember the link) */
 const injectCSS = (css) => {
-    let el = document.createElement("style")
-    el.type = "text/css"
-    el.innerText = css
-    document.head.appendChild(el)
-    return el
+    const styleElement = document.createElement("style")
+    styleElement.type = "text/css"
+    styleElement.innerText = css
+    document.head.appendChild(styleElement)
+    return styleElement
 }
 
-/** Slider class */
+injectCSS(`
+    .custom-slider {
+        display: flex;
+        overflow-x: hidden;
+        scroll-snap-type: x mandatory;
+        -webkit-scroll-snap-type: x mandatory;
+        scroll-behavior: smooth;
+    }
+    
+    .custom-slider img {
+        width: 75%;
+        scroll-snap-align: center;
+        -webkit-scroll-snap-align: center;
+        object-fit: cover;
+    }
+`)
+
 export class Slider {
-    /**
-     *
-     * @param {string} selector - The selector for a slider div or section
-     * @param {string[i]} images
-     */
+    #imageCallbacks = []
+
     constructor(selector, images) {
         if (
             !selector ||
             !images ||
             !Array.isArray(images) ||
-            images.length < 0
+            images.length === 0
         ) {
-            throw new Error("You need to specify the arguments correctly")
+            throw new Error("You need to specify the arguments correctly.")
         }
 
         this.parentElement = document.querySelector(selector)
-
         if (!this.parentElement) {
-            throw new Error("Could not find element")
+            throw new Error("Could not find element.")
         }
 
         this.images = images
-        this.imageObjects = []
+        this.imageElements = []
+        this.currentImageIndex = 0
 
         this.#init()
     }
 
+    addImageIndexCallback(callback) {
+        this.#imageCallbacks.push(callback)
+    }
+
     #init() {
-        injectCSS(`
-            .custom-slider {
-                display: flex;
-                overflow-x: hidden;
-
-                scroll-snap-type: x mandatory;
-                -webkit-scroll-snap-type: x mandatory;
-                scroll-behavior: smooth;
-
-                img {
-                    width: 100%;
-                    scroll-snap-align: center;
-                    -webkit-scroll-snap-align: center;
-
-                    object-fit: cover;
-                }
-            }
-            `)
-
         this.parentElement.classList.add("custom-slider")
-        console.log(this.parentElement)
 
-        this.images.forEach((image, i) => {
-            const imageElement = document.createElement("img")
+        const fragment = document.createDocumentFragment()
 
-            imageElement.src = image
+        this.#createImageElement(this.images[1], fragment)
+        this.#createImageElement(this.images[this.images.length - 1], fragment)
+        this.images.forEach((image) =>
+            this.#createImageElement(image, fragment)
+        )
+        this.#createImageElement(this.images[0], fragment)
+        this.#createImageElement(this.images[1], fragment)
 
-            this.parentElement.appendChild(imageElement)
+        this.parentElement.appendChild(fragment)
 
-            this.imageObjects.push(imageElement)
+        window.addEventListener("load", () => {
+            this.#resetScroll(
+                this.imageElements[0].offsetWidth +
+                    this.imageElements[1].offsetWidth
+            )
         })
+    }
 
-        const imageElement = document.createElement("img")
-
-        imageElement.src = this.images[0]
-
-        this.parentElement.appendChild(imageElement)
-
-        this.imageObjects.push(imageElement)
+    #createImageElement(src, fragment) {
+        const imgElement = document.createElement("img")
+        imgElement.src = src
+        fragment.appendChild(imgElement)
+        this.imageElements.push(imgElement)
     }
 
     nextSlide() {
-        if (
-            this.parentElement.scrollLeft +
-                this.imageObjects[this.imageObjects.length - 1].offsetWidth ==
-            this.parentElement.scrollWidth
-        ) {
-            this.parentElement.style.scrollBehavior = "auto"
-            this.parentElement.scrollLeft = 0
-            this.parentElement.style.scrollBehavior = "smooth"
+        const slideWidth = this.imageElements[0].offsetWidth
 
-            this.nextSlide()
+        if (
+            this.parentElement.scrollLeft >=
+            this.parentElement.scrollWidth - slideWidth * 4
+        ) {
+            const resetPosition = slideWidth * 1
+            this.#resetScroll(resetPosition)
+            this.parentElement.scrollLeft += slideWidth
         } else {
-            this.parentElement.scrollLeft += this.imageObjects[0].offsetWidth
+            this.parentElement.scrollLeft += slideWidth
         }
+
+        this.#setImageIndex((this.currentImageIndex + 1) % this.images.length)
     }
 
     prevSlide() {
-        if (this.parentElement.scrollLeft == 0) {
-            this.parentElement.style.scrollBehavior = "auto"
-            this.parentElement.scrollLeft =
-                this.parentElement.scrollWidth -
-                this.imageObjects[this.imageObjects.length - 1].offsetWidth
-            this.parentElement.style.scrollBehavior = "smooth"
+        const slideWidth = this.imageElements[0].offsetWidth
 
-            this.prevSlide()
+        if (this.parentElement.scrollLeft <= slideWidth * 1) {
+            const resetPosition =
+                this.parentElement.scrollWidth - slideWidth * 3
+            this.#resetScroll(resetPosition)
+            this.parentElement.scrollLeft -= slideWidth
         } else {
-            this.parentElement.scrollLeft -= this.imageObjects[0].offsetWidth
+            this.parentElement.scrollLeft -= slideWidth
         }
+
+        this.#setImageIndex(
+            (this.currentImageIndex - 1 + this.images.length) %
+                this.images.length
+        )
+    }
+
+    #setImageIndex(index) {
+        this.currentImageIndex = index
+
+        this.#imageCallbacks.forEach((callback) =>
+            callback(this.currentImageIndex)
+        )
+    }
+
+    #resetScroll(position) {
+        this.parentElement.style.scrollBehavior = "auto"
+        this.parentElement.scrollLeft = position
+        this.parentElement.style.scrollBehavior = "smooth"
     }
 }
